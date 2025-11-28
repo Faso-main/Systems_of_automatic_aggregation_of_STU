b11b97555f027a8c6ea3a84b7f9b75d9bec8e106
Faso-main <faso.312@yandex.ru>
2025-11-29 00:52:06 +0400

    fix : file removal

--- py_back/rexexp/import_2.py
+++ py_back/rexexp/import_2.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # Простая конвертация CSV в Excel
 df = pd.read_csv('py_back/rexexp/data/result_itr4.csv')
-df.to_excel('py_back/rexexp/data/final_for_import.xlsx', index=False)
+df.to_excel('py_back/rexexp/data/result_itr4.xlsx', index=False)
 
 print("✅ CSV успешно конвертирован в Excel")
