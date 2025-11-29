f6a8d00b7bdf718bee4ad46ad2a245bc3fd0cc6e
Faso-main <faso.312@yandex.ru>
2025-11-29 12:33:16 +0400

    update : minor

--- py_back/rexexp/test_llm_itr1.py
+++ py_back/rexexp/test_llm_itr1.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import json
 from typing import Dict, Any, List
 
-from py_back.rexexp.runtime_llm_itr2 import RuntimeCharacteristicsExtractor
+from runtime_llm_itr2 import RuntimeCharacteristicsExtractor
 
 
 OUTPUT_TESTS_PATH = "runtime_llm_tests.json"
